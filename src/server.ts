a3db52f303b8ae92bf9cee42e6b900f6fbdaa9cf
raulfranck <raulengfranck@gmail.com>
2022-01-08 23:42:40 -0300

    configurando socket

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,28 @@ import express from "express";
 import http from "http";
 import socketIO from "socket.io";
 
-
 const app = express();
 const httpServer = http.createServer(app);
 
-const io = new socketIO.Server(httpServer);
+const io = new socketIO.Server(httpServer, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hey Socket.io</h1>');
+});
 
 io.on('connection', (socket) => {
     console.log("New connection :" + socket.id)
+    socket.emit("teste", "novo dado")
 })
+
+io.on('new-message', (message) => {
+    io.emit(message);
+});
 
 httpServer.listen(3000, () => {
     console.log("rodando na porta : 3000")
